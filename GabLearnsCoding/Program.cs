107d9de71d2f4dba89105017b3e17516af74589e
Gigá Tchad <thetopkekboy@gmail.com>
2021-09-16 14:23:39 +0200

    CHanged Hello world to Hello Deinname

--- GabLearnsCoding/Program.cs
+++ GabLearnsCoding/Program.cs
@@ -6,7 +6,7 @@ namespace GabLearnsCoding
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello Deinname!");
         }
     }
 }
