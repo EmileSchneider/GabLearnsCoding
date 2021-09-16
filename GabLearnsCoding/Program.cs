965170e139aaf2dbca94c02ba83707bed543cdd9
emileschneider <emileschneider7@gmail.com>
2021-09-16 14:05:31 +0200

    Add project files.

---
+++ GabLearnsCoding/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GabLearnsCoding
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
